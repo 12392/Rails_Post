360394db73c15408d872746af7e3527f192d1411
suryanshgupta133@gmail.com <suryanshgupta133@gmail.com>
2021-10-29 17:25:31 +0530

    [ADD] SMTP mail

--- app/mailers/post_mailer.rb
+++ app/mailers/post_mailer.rb
@@ -2,7 +2,7 @@ class PostMailer < ApplicationMailer
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
-  #
+  # User.where.not(id: @user.id).pluck(:email), 
   #   en.post_mailer.post_created.subject
   #
   def post_created(user,post)
@@ -12,7 +12,7 @@ class PostMailer < ApplicationMailer
 
     mail(
       from: "support@postApp.com",
-      to:   User.where.not(id: @user.id).pluck(:email), 
+      to:   "		rajatbajpai33@gmail.com",
       subject: "New Post Created"
     ) 
   end
